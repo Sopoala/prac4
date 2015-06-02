0bb8b0171af3f0b1392ae0ed36138b87e52b7dca
Randy Zhongbin Li <sopoala@gmail.com>
2015-06-03 00:02:34 +1000

    visitor counter

---
+++ visits.php
@@ -0,0 +1,28 @@
+<meta http-equiv="Refresh" content="2">
+<html>
+<head>
+    <title>Applcation object in JSP</title>
+</head>
+<body>
+
+<?php
+$count_my_page = ("hitcounter.txt");
+if (!file_exists($count_my_page)) {
+    $f = fopen($count_my_page, "w");
+    fwrite($f,"0");
+    fclose($f);
+}
+$hits = file($count_my_page);
+$hits[0] ++;
+$fp = fopen($count_my_page , "w");
+fputs($fp , "$hits[0]");
+fclose($fp);
+?>
+    <p><h1>Total number of visits: <?php echo $hits[0]; ?></></p>
+    <p><h2> Auto Refresh Every 2 Seconds</h2></p>
+    <p><h3>Current Time: <?php date_default_timezone_set('Australia/Melbourne');
+        $date = date('m/d/Y h:i:s a', time());
+        echo $date;?></h3></p>
+
+</body>
+</html>
